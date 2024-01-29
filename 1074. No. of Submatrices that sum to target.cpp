e2eb969e190c066d925db5e49e6a97570a44e751
Vaibhav <vaibz.work@gmail.com>
2024-01-29 20:25:20 +0530

    Update 1074. No. of Submatrices that sum to target.cpp

--- 1074. No. of Submatrices that sum to target.cpp
+++ 1074. No. of Submatrices that sum to target.cpp
@@ -1,10 +1,18 @@
+// Topics -> Matrix. Subarrays
 
+// Given a matrix and a target, return the number of non-empty submatrices that sum to target.
 
+// A submatrix x1, y1, x2, y2 is the set of all cells matrix[x][y] with x1 <= x <= x2 and y1 <= y <= y2.
 
+// Two submatrices (x1, y1, x2, y2) and (x1', y1', x2', y2') are different if they have some coordinate that is different: for example, if x1 != x1'.
 
+// Input: matrix = [[0,1,0],[1,1,1],[0,1,0]], target = 0
+// Output: 4
+// Explanation: The four 1x1 submatrices that only contain 0.
 
 class Solution {
 public:
+	//This func is from prob no 560 -> subarray sum (1 D)
     int subarraySum(vector<int>& nums, int k) {
         int count=0,sum=0;
         map<int,int>mp;
@@ -27,12 +35,17 @@ public:
        int count=0;
         for(int i=0; i<matrix.size(); i++){
 			
-            vector<int> sum(matrix[0].size(), 0);
+            vector<int> sum(matrix[0].size(), 0);   //Starts afresh from each row.
+		
             for(int j=i; j<matrix.size(); j++){
                 for(int k=0; k<matrix[0].size(); k++){
+			
                     sum[k] += matrix[j][k];
+			//This sum is basically the current row, after summing uske upar wale elements into that cell in each column. 
+			//So, uske upar wale saare permutations will covered in it
+			
                 }
-                count += subarraySum(sum, target);
+                count += subarraySum(sum, target); //running subarray sum for that row, covers all permutations
             }
         }
        
